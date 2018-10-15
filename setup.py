085bc3528fe5545bf2945b61e729e0d398dbb1a2
Weitang Li <liwt31@163.com>
2018-10-15 22:42:18 +0800

    fix python2 bug open decode bug

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import setuptools
 
-with open('README.md', encoding='utf-8') as fin:
-    long_description = fin.read()
+with open('README.md', 'rb') as fin:
+    long_description = fin.read().decode('utf-8')
 
 setuptools.setup(
     name='print_tree2',
+    version='0.9.4',
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
